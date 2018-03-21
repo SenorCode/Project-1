11da9b5ed1f5001dee95123aa6b9ab8f50ee4820
ballardcz <ballardcz@gmail.com>
2018-03-21 15:31:16 -0400

    trying to fic up the geolocating

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -25,7 +26,8 @@ var map, infoWindow;
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-      }
+        displayCords();
+     }
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
@@ -33,4 +35,46 @@ var map, infoWindow;
                               'Error: The Geolocation service failed.' :
                               'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
+        console.log()
       }
+
+ function displayCords(){
+  console.log(map);
+}
+
+
+// var geocoder;
+//   var map;
+//   function initialize() {
+//     geocoder = new google.maps.Geocoder();
+//     var latlng = new google.maps.LatLng(-34.397, 150.644);
+//     var mapOptions = {
+//       zoom: 8,
+//       center: latlng
+//     }
+//     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//   }
+
+//   function codeAddress() {
+//     var address = document.getElementById('address').value;
+//     geocoder.geocode( { 'address': address}, function(results, status) {
+//       if (status == 'OK') {
+//         map.setCenter(results[0].geometry.location);
+//         var marker = new google.maps.Marker({
+//             map: map,
+//             position: results[0].geometry.location
+//         });
+//       } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//       }
+//     });
+//   }
+// console.log(map);
+// console.log(initialize.latlng)
+//$( document ).ready(displayCords);
+
+
+      // GET https://api.twitter.com/1.1/trends/place.json?id=1
+
+    // k5WrjArSp7K4VIvmtGIoYpJ7Y 
+    // 2BNgVWygkLvL1CtIIHyyQdNgTTMf7X9O4j3h1JbAqaLxULtNON
